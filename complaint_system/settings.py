6dc60769fc73fa17678cde6b8a6db7887eedcc95
Adarsh Priya kashyap <2102004_cse@gita.edu.in>
2026-03-07 22:58:10 +0530

    create admin user

--- complaint_system/settings.py
+++ complaint_system/settings.py
@@ -20,9 +20,20 @@ from pathlib import Path
 
 
 
-
-
 from datetime import timedelta
+
+
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+
+
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
