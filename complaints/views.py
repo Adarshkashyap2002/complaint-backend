8b7391302765eee8cdb4314e38a692ad3766a5df
Adarsh Priya kashyap <2102004_cse@gita.edu.in>
2026-03-07 17:38:38 +0530

    backend deployment

---
+++ complaints/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import Complaint
+from .serializers import ComplaintSerializer
+
+class ComplaintViewSet(viewsets.ModelViewSet):
+    queryset = Complaint.objects.all()
+    serializer_class = ComplaintSerializer
